dd2da854facda3618f8dac11abca31f021512602
hugopinto1997 <hugopillo@hotmail.com>
2018-05-20 00:11:02 -0600

    Initial commit

---
+++ app/src/main/java/com/hugopinto/srn/Datos/Persona.java
@@ -0,0 +1,40 @@
+package com.hugopinto.srn.Datos;
+
+public class Persona {
+    private String carnet;
+    private String nota;
+    private String materia;
+    private String catedratico;
+
+    public String getCarnet() {
+        return carnet;
+    }
+
+    public void setCarnet(String carnet) {
+        this.carnet = carnet;
+    }
+
+    public String getNota() {
+        return nota;
+    }
+
+    public void setNota(String nota) {
+        this.nota = nota;
+    }
+
+    public String getMateria() {
+        return materia;
+    }
+
+    public void setMateria(String materia) {
+        this.materia = materia;
+    }
+
+    public String getCatedratico() {
+        return catedratico;
+    }
+
+    public void setCatedratico(String catedratico) {
+        this.catedratico = catedratico;
+    }
+}
